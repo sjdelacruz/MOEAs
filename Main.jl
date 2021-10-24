3ca7757423a374275c093a025145c51d215baad8
Jesus Mejia <jesusmejded@gmail.com>
2021-10-24 14:32:20 -0500

    testing on C1_DTLZ3

--- Main.jl
+++ Main.jl
@@ -1,17 +1,18 @@
 using Distances
 using Metaheuristics
+using Plots;pyplot()
 
 try
     using HardTestProblems
 catch
     # Install module
-    import Pkg; Pkg.add(url="https://github.com/jmejia8/HardTestProblems.jl")
+    import Pkg; Pkg.add("HardTestProblems")
 end
 using HardTestProblems
 
 include("CTAEA.jl")
 
-function main()
+function main_old()
     
     #Objective function and a set of parameters
     f, conf = get_RW_MOP_problem(3)
@@ -40,5 +41,31 @@ function main()
     #fvals(resultado)
 
 end
+
+function main() 
+
+    f, bounds, front = Metaheuristics.TestProblems.C1_DTLZ3()
+
+    M = 3
+    options = Options(iterations=1000, debug=false)
+
+    # reference points (Das and Dennis's method)
+    weights = gen_ref_dirs(M, 12)
+
+    # algoritmo a utilizar
+    algorithm = CTAEA(weights, N = length(weights), η_cr = 30, η_m = 20, options = options)
+
+    # Start optimization process
+    resultado = optimize(f, bounds, algorithm)
+    display(resultado)
+
+
+    fs = fvals(front)
+    wireframe(fs[:,1], fs[:,2], fs[:,3])
+    fs = fvals(resultado)
+    scatter!(fs[:,1], fs[:,2], fs[:,3])
+    #fvals(resultado)
+end
+
 
 main()
